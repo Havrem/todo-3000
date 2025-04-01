f801af364054fb6e8966ddc4b877d3a12c0a3afc
Havrem <144966173+Havrem@users.noreply.github.com>
2025-04-01 04:08:38 +0200

    Add allowed cors origin

--- src/main/java/com/havrem/todo/configs/SecurityConfig.java
+++ src/main/java/com/havrem/todo/configs/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://todo-3000-frontend.vercel.app/todos"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
