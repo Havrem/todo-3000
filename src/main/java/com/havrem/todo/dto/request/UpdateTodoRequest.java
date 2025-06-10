b18f2274b309f9bb4ccc14827b261585ea13c3b8
Havrem <144966173+Havrem@users.noreply.github.com>
2025-06-10 06:26:28 +0200

    Fix validation on requests

--- src/main/java/com/havrem/todo/dto/request/UpdateTodoRequest.java
+++ src/main/java/com/havrem/todo/dto/request/UpdateTodoRequest.java
@@ -6,7 +6,7 @@ import java.time.LocalDate;
 
 public record UpdateTodoRequest(
         @Size(max = 100, message = "TITLE_TOO_LONG") String title,
-        @Size(max = 20, message = "DESCRIPTION_TOO_LONG") String description,
+        @Size(max = 200, message = "DESCRIPTION_TOO_LONG") String description,
         LocalDate due,
         boolean completed
 ) {}
